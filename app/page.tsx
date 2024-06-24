af7542c92a9c160db0e8b011064ef4faf085986a
Shardul Bansal <33334493+bansal19@users.noreply.github.com>
2024-06-24 16:10:45 -0700

    about me

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Hey 👋, I'm Shardul
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`- Grew up in 6 different countries
+          - CS @ U of Toronto.
+          - Founding engineer of 2 startups during my degree 1) Bibbit, a platform for authors to gain traction so publishers don't hold all the power and 2) emotiv, a music classification app that categorises music by emotion rather than specific genres.
+          - Created benchmarking software for ML models that were trained in decentralized, check out [bittensor.com](http://bittensor.com/)
+          - Worked at AWS Cloud Map where my main focus was on scaling
+          - Building something new in climate-tech.`}
       </p>
       <div className="my-8">
         <BlogPosts />
